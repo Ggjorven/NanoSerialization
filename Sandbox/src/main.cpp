a62dee3cb40f6c70b89d5b2045d85a2468235bf1
ggjorven <jorbenvanderwal@gmail.com>
2025-07-15 23:10:02 +0200

    Remove toml files.

--- Sandbox/src/main.cpp
+++ Sandbox/src/main.cpp
@@ -3,10 +3,6 @@
 
 #include "NanoSerialization/Core/Core.hpp"
 
-#include "NanoSerialization/Toml/Toml.hpp"
-#include "NanoSerialization/Toml/File.hpp"
-#include "NanoSerialization/Toml/Node.hpp"
-
 using namespace Nano::Serialization;
 
 using namespace std::literals;
@@ -15,28 +11,7 @@ int main(int argc, char* argv[])
 {
     (void)argc; (void)argv;
 
-    // Input
-    {
-        Toml::File file(std::filesystem::path("input.toml"));
+    std::cout << "Hello, world!" << std::endl;
 
-        std::string_view libraryName = file["library"]["name"].As<std::string_view>().value();
-        std::string_view libraryAuthor = file["library"]["authors"][0].As<std::string_view>().value();
-        int64_t cppVersion = file["dependencies"]["cpp"].As<int>().value();
-
-        std::cout << "Library Name: " << libraryName << std::endl;
-        std::cout << "Library Author: " << libraryAuthor << std::endl;
-        std::cout << "Cpp Version: " << cppVersion << std::endl;
-    }
-
-    // Output
-    {
-        Toml::File file;
-        Toml::Node& fileNode = file;
-
-        fileNode << Toml::NodeType::Key << "First" << Toml::NodeType::Value << "String";
-
-        std::ofstream out(std::filesystem::path("output.toml"));
-        out << file.AsString();
-        out.close();
-    }
+    return 0;
 }
